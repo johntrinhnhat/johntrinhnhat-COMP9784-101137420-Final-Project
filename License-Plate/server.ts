8f06f19547cf51869aa8307ea591530b556e1808
KHOITRINH03 <Johntrinhnhat@gmail.com>
2025-03-22 12:46:34 -0400

    add server.ts

--- License-Plate/server.ts
+++ License-Plate/server.ts
@@ -2,15 +2,26 @@ import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
 import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
 import { connectDB } from "../User-Sign-Up/config/db";
-// import { router } from "./routes/authRoutes";
+import { router } from "../License-Plate/routes/licenseRoutes";
 
+const RateLimit = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
 const app = express();
+
 connectDB();
 
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(router);
+app.use(RateLimit);
+app.use(router);
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.listen(PORT, () => {
