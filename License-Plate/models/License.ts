086bbf12f2fe87b6b99e8a5d4575d00a7402d92d
KHOITRINH03 <Johntrinhnhat@gmail.com>
2025-03-24 12:33:46 -0400

    update license models

--- License-Plate/models/License.ts
+++ License-Plate/models/License.ts
@@ -1,22 +1,20 @@
 import mongoose from "mongoose";
 
-const LicenseSchema = new mongoose.Schema({
-  licensePlate: {
-    type: String,
-    require: true,
-    unique: true,
-    match: /^[A-Z]{4}[0-9]{3}$/, // 4 uppercase letters followed by 3 digits
+const LPSchema = new mongoose.Schema(
+  {
+    licensePlate: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["available", "revoked", "assigned"],
+      default: "available",
+    },
   },
-  vin: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^[A-HJ-NPR-Z0-9]{17}$/, // 17 in length, alpha numeric but not {I, O, Q}
-  },
-  dateCreated: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { collection: "License" }
+);
 
-export const License = mongoose.model("License", LicenseSchema);
+export const LP = mongoose.model("LP", LPSchema);
